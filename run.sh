3eaf962f935822512da4b87ff5da4c59c5e654a4
Bincom <deezplay2003@gmail.com>
2023-03-25 00:02:26 +0300

    run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,6 @@
+wget https://data.metabrainz.org/pub/musicbrainz/data/fullexport/20230322-002138/mbdump.tar.bz2
+tar xvf mbdump.tar.bz2
+mv mbdump/artist ./artist
 cmake build .
 make
 ./dz1 ./artist 1970 1970
